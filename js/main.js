34f457f38d63fb689f7d81d8af3bf62906e40ce4
Abdesslame Abouibrahim <aabouibr@e1r6p10.1337.ma>
2019-11-28 20:12:06 +0100

    volume reduced

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var audio = new Audio('audio_file.mp3');
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
+      audio.volume = 0.2;
       audio.play();
       audio.loop = true;
       video.srcObject = stream;
